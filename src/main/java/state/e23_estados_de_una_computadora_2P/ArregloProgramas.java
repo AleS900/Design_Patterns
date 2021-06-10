afd26cd3cb82fe8c6f0a1c5a35dd1c9a72f5b15a
AleS900 <santiavctf@gmail.com>
2021-06-10 10:11:51 -0400

    Tarea 16 Ejercicio STATE

--- src/main/java/state/e23_estados_de_una_computadora_2P/ArregloProgramas.java
+++ src/main/java/state/e23_estados_de_una_computadora_2P/ArregloProgramas.java
@@ -1,11 +1,8 @@
 package state.e23_estados_de_una_computadora_2P;
 
-import java.util.Map;
-
 public class ArregloProgramas {
     private String arr_id;
     private Programa[] arr_data;
-    private Map<Integer, Boolean>[] list_id;
 
     public ArregloProgramas(String arr_id, Programa[] arr_data) {
         this.arr_id = arr_id;
@@ -30,7 +27,6 @@ public class ArregloProgramas {
 
     public void showInfo(int number){
         for(int i=0; i<number; i++){
-            int numero = (int)(Math.random() * number+1);
                 System.out.println("Programa abierto: " + arr_data[(int)(Math.random()*number+1)].getProg_name());
         }
         System.out.println("*********************\n");
