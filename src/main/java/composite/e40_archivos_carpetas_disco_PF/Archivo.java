c00d99e80978afd718ef82e6e98521945b6d0116
AleS900 <santiavctf@gmail.com>
2021-06-28 03:50:09 -0400

    Tarea 23 Ejercicios PARCIAL FINAL Composite

---
+++ src/main/java/composite/e40_archivos_carpetas_disco_PF/Archivo.java
@@ -0,0 +1,35 @@
+package composite.e40_archivos_carpetas_disco_PF;
+
+public class Archivo extends Almacenamiento{
+
+
+    public Archivo(String stge_id, String extension, String txt_body, String type) {
+        super(stge_id, extension, txt_body, type);
+    }
+
+    public Archivo(String stge_id, String extension, int words_number, String type) {
+        super(stge_id, extension, words_number, type);
+    }
+
+
+
+    @Override
+    public void countWords() {
+        this.getWordsNumber();
+    }
+
+    @Override
+    public void add(Almacenamiento storge) {
+        System.out.println("No applicable");
+    }
+
+    @Override
+    public void remove(Almacenamiento storge) {
+        System.out.println("No applicable");
+    }
+
+    @Override
+    public void getChild(int position) {
+        System.out.println("No applicable");
+    }
+}
---
+++ src/main/java/composite/e40_archivos_carpetas_disco_PF/Global.java
@@ -0,0 +1,5 @@
+package composite.e40_archivos_carpetas_disco_PF;
+
+public class Global {
+    protected static int total_words = 0;
+}
