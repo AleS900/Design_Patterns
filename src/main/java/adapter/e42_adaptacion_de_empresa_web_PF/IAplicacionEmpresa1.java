006bd499efdd2c3b63eec385a59402aa17818c10
AleS900 <santiavctf@gmail.com>
2021-06-28 06:08:22 -0400

    Tarea 23 Ejercicios PARCIAL FINAL ADAPTER

---
+++ src/main/java/adapter/e42_adaptacion_de_empresa_web_PF/IAplicacionEmpresa1.java
@@ -0,0 +1,7 @@
+package adapter.e42_adaptacion_de_empresa_web_PF;
+
+public interface IAplicacionEmpresa1 {
+    void login();
+    void logout();
+    void reportes();
+}
---
+++ src/main/java/adapter/e42_adaptacion_de_empresa_web_PF/IAplicacionEmpresa2.java
@@ -0,0 +1,7 @@
+package adapter.e42_adaptacion_de_empresa_web_PF;
+
+public interface IAplicacionEmpresa2 {
+    void iniciarSesion();
+    void cerrarSesion();
+    void generarDatos();
+}
